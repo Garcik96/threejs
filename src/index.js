78de7b8ac811c7d1fb855831b5a9ff5c6cc34b90
Ivan Garcia Ballesteros <garcia.ballesteros96@gmail.com>
2021-05-11 14:51:39 +0200

    Añadidos comentarios y cambios pequeños

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import "@babel/polyfill";
 import './styles.scss';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
-import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
 
 (async() => {
     /* Tamaño de la escena */
@@ -17,9 +17,15 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
     /* Posición cámara principal */
     const mainCameraPosition = Object.freeze({
         x: 0,
-        y: 5,
-        z: 25,
+        y: 7,
+        z: 21,
     });
+
+    /* */
+    const mainCameraRotationX = Math.PI * -.1;
+
+    /* */
+    const firstPersonCameraRotationX = 0;
 
     /* Tipos de cámara */
     const CameraType = Object.freeze({
@@ -38,6 +44,13 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
 
     /* Vehículo */
     let car;
+
+    /* */
+    const carSize = Object.freeze({
+        x: 0.01,
+        y: 0.01,
+        z: 0.01,
+    });
 
     /* Dirección del vehículo */
     let carDirection = 0;
@@ -70,6 +83,7 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
     /* Cámara y posicionamiento inicial de la misma */
     const camera = new THREE.PerspectiveCamera(45, sceneSize.width / sceneSize.height, 0.1, 1000);
     camera.position.set(mainCameraPosition.x, mainCameraPosition.y, mainCameraPosition.z);
+    camera.rotation.x = mainCameraRotationX;
 
     /* Escena y asignación del color de fondo */
     const scene = new THREE.Scene();
@@ -125,10 +139,10 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
     const response = await fetch('assets/parkings.json');
     const parkingsData = await response.json();
 
-    /* */
+    /* Cargador de fuente */
     const fontLoader = new THREE.FontLoader();
 
-    /* */
+    /* Material de los textos de plazas libres en función del % de plazas ocupadas */
     const materialTextParkingPlace = (freePlaces, totalPlaces) => {
         const textRed = 0xF44336;
         const textOrange = 0xFF9800;
@@ -152,7 +166,7 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
         });
     };
 
-    /* */
+    /* Creación de los objetos para textos de plazas libres */
     const textParkingPlaces = (font, nFreePlaces, nTotalPlaces, randomXPosition, randomZPosition, index, nRemove) => {
         const freePlaces = nFreePlaces;
         const shapesParkingPlaces = font.generateShapes(freePlaces, 0.25);
@@ -168,7 +182,7 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
         scene.add(textParkingPlaces);
     }
 
-    /* */
+    /* Creación de los objetos parking*/
     for(let i = 0; i < parkingsData.resources.length; i++) {
 
         let randomXPosition = random(-(planeSize - parkingSize) / 2, (planeSize - parkingSize) / 2);
@@ -218,12 +232,12 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
         materials.preload();
         new OBJLoader().setMaterials(materials).load('./assets/Car.obj', (object) => {
             car = object;
-            car.scale.set(0.01, 0.01, 0.01);
+            car.scale.set(carSize.x, carSize.y, carSize.z);
             car.position.set(carStartingPosition.x, carStartingPosition.y, carStartingPosition.z);
             car.rotation.y = Math.PI;
             scene.add(car);
         });
-    } );
+    });
 
     /* Registra el evento 'resize' producido cuando se cambia el tamaño de ventana */
     window.addEventListener('resize', () => {
@@ -272,6 +286,7 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
                 selectedCamera = CameraType.MainCamera;
                 camera.position.set(mainCameraPosition.x, mainCameraPosition.y, mainCameraPosition.z);
                 camera.rotation.y = 0;
+                camera.rotation.x = mainCameraRotationX;
             }
         }
     });
@@ -293,14 +308,14 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
         }
     };
 
-    /* */
+    /* Controlador de colisiones entre vehiculo y parkings*/
     const collisionControler = () => {
         if(car) {
             parkings.forEach((parking, index) => {
                 if(parking.position.x - parkingSize / 2 <= car.position.x && parking.position.x + parkingSize / 2 >= car.position.x && parking.position.z - parkingSize / 2 <= car.position.z && parking.position.z + parkingSize / 2 >= car.position.z) {
                     /* */
                     scene.remove(textsParkingsPlaces[index]);
-                    // /* */
+                    /* */
                     car.position.set(carStartingPosition.x, carStartingPosition.y, carStartingPosition.z);
                     carAngle = 180;
                     carDirection = 0;
@@ -314,7 +329,7 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
                         parkings.splice(index, 1);
                         return;
                     }
-                    
+
                     /* */
                     fontLoader.load('assets/helvetiker_regular.typeface.json', (font) => {
                         textParkingPlaces(font, parkingsData.resources[index].plazaslib + "", parkingsData.resources[index].plazastot, parking.position.x, parking.position.z, index, 1);
@@ -324,14 +339,16 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
         }
     };
 
-    /* Actualización de la posición de la cámara en función de la seleccionada*/
+    /* Controlador de camara */
     const cameraController = () => {
         if(selectedCamera === CameraType.FirstPersonCamera) {
             camera.position.set(car.position.x, car.position.y + 0.4, car.position.z);
             camera.rotation.y = Math.PI / 180 * firstPersonCameraAngle;
+            camera.rotation.x = firstPersonCameraRotationX;
         }
     };
 
+    /* Controlador de parkings */
     const parkingsController = () => {
         /* Rotación de todos los parkings sobre el eje 'Y' */
         parkings.forEach(parking => {
@@ -351,7 +368,9 @@ import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
 
     /* Animación de la escena */
     const animate = () => {
-        /* Solicita que el navegador programe el repintado de la ventana para el próximo ciclo de animación */
+        /* Solicita que el navegador programe 
+        el repintado de la ventana para el 
+        próximo ciclo de animación */
         requestAnimationFrame(animate);
         render();
     };
